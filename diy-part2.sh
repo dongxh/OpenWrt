a188ad88a642b993f08cc3257195883f12113506
dongxh <dxh8899@msn.com>
2022-06-11 06:14:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,7 @@
 sed -i 's/192.168.1.2/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 2.清除默认密码/改密码为 
-sed -i 'dxh2116277' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 5.修改默认主题
+# sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
