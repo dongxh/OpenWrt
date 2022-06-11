fc30af4d1d4c40b89a7f81797f817c508b721b1c
dongxh <dxh8899@msn.com>
2022-06-11 14:58:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 #sed -i 's/192.168.1.2/192.168.50.5/g' package/base-files/files/bin/config_generate
 # 1.更改默认IP
-sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.2/192.168.0.2/g' package/base-files/files/bin/config_generate
 
 # 2.清除默认密码/改密码为 
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
